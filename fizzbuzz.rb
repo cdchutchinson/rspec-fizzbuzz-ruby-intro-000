bd731fe1652d654144effcb963f7f4e43b71e3c3
Crystal <cdchutchinson@gmail.com>
2020-03-07 17:08:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
-fizzbuzz(3) == "Fizz"
+def fizzbuzz(int)
+  if int % 3 == 0 and int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 3 == 0
+    return  "Fizz"
+
+  end
+
 end
